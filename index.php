87ad162128de7e376a2b4ae75d2f7abe87425cb5
Oldrado Junior <oldradojunior@gmail.com>
2020-05-17 03:49:20 -0300

    Criando tela de login na administração

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
+session_start();
 
 require_once 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
+use Hcode\Model\User;
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
@@ -10,14 +12,35 @@ $app = new Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
+$app->get('/', function () {
 	$page = new Page();
 	$page->setTpl('index');
 });
 
-$app->get('/admin', function() {
+$app->get('/admin', function () {
+	User::verifyLogin();
 	$page = new PageAdmin();
 	$page->setTpl('index');
+});
+
+$app->get('/admin/login', function () {
+	$page = new PageAdmin([
+		'header' => false,
+		'footer' => false
+	]);
+	$page->setTpl('login');
+});
+
+$app->post('/admin/login', function () {
+	User::login($_POST['login'], $_POST['password']);
+	header('Location: /admin');
+	exit;
+});
+
+$app->get('/admin/logout', function () {
+	User::logout();
+	header('Location: /admin/login');
+	exit;
 });
 
 $app->run();
