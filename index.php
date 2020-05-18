bd28692b5981ab38fc8b5e5095e013804b2b6e31
Oldrado Junior <oldradojunior@gmail.com>
2020-05-17 23:18:41 -0300

    Criando CRUD para usuários na admin

--- index.php
+++ index.php
@@ -43,4 +43,69 @@ $app->get('/admin/logout', function () {
 	exit;
 });
 
+$app->get('/admin/users', function () {
+	User::verifyLogin();
+	$users = User::listAll();
+	$page = new PageAdmin();
+	$page->setTpl('users', ['users' => $users]);
+});
+
+$app->get('/admin/users/create', function () {
+	User::verifyLogin();
+	$page = new PageAdmin();
+	$page->setTpl('users-create');
+});
+
+$app->get('/admin/users/:iduser/delete', function ($idUser) {
+	User::verifyLogin();
+
+	$user = new User();
+	$user->get($idUser);
+	$user->delete();
+
+	header('Location: /admin/users');
+	exit;
+});
+
+$app->get('/admin/users/:iduser', function ($idUser) {
+	User::verifyLogin();
+
+	$user = new User();
+	$user->get($idUser);
+
+	$page = new PageAdmin();
+	$page->setTpl('users-update', ['user' => $user->getValues()]);
+});
+
+$app->post('/admin/users/create', function () {
+	User::verifyLogin();
+
+	if (!isset($_POST['inadmin'])) {
+		$_POST['inadmin'] = 0;
+	}
+
+	$user = new User();
+	$user->setData($_POST);
+	$user->save();
+
+	header('Location: /admin/users');
+	exit;
+});
+
+$app->post('/admin/users/:iduser', function ($idUser) {
+	User::verifyLogin();
+
+	if (!isset($_POST['inadmin'])) {
+		$_POST['inadmin'] = 0;
+	}
+
+	$user = new User();
+	$user->get($idUser);
+	$user->setData($_POST);
+	$user->update();
+
+	header('Location: /admin/users');
+	exit;
+});
+
 $app->run();
