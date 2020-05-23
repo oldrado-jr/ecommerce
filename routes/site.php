744fb2c0046c66d137d61e6dc7566b9c8b2b85ac
Oldrado Junior <oldradojunior@gmail.com>
2020-05-23 19:40:03 -0300

    Adiciona paginação no site

--- routes/site.php
+++ routes/site.php
@@ -6,15 +6,29 @@ use Hcode\Page;
 
 $app->get('/', function () {
 	$page = new Page();
-	$page->setTpl('index', ['products' => Product::checklist(Product::listAll())]);
+	$page->setTpl('index', ['products' => Product::listAll()]);
 });
 
 $app->get('/categories/:idcategory', function ($idCategory) {
 	$category = new Category();
 	$category->get($idCategory);
+
+	$page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
+	$pagination = $category->getProductsPage($page);
+
+	$pages = [];
+
+	for ($i = 1; $i <= $pagination['pages']; $i++) {
+		$pages[] = [
+			'link' => '/categories/' . $category->getIdcategory() . '?page=' . $i,
+			'page' => $i
+		];
+	}
+
 	$page = new Page();
 	$page->setTpl('category', [
 		'category' => $category->getValues(),
-		'products' => Product::checklist($category->getProducts())
+		'products' => $pagination['data'],
+		'pages' => $pages
 	]);
 });
