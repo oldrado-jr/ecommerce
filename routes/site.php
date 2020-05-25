abeaa69bf3ec6e150a3a37ba625fee2539fdc400
Oldrado Junior <oldradojunior@gmail.com>
2020-05-24 23:07:23 -0300

    Adiciona cálculo de frete no Carrinho

--- routes/site.php
+++ routes/site.php
@@ -50,7 +50,8 @@ $app->get('/cart', function () {
 	$page = new Page();
 	$page->setTpl('cart', [
 		'cart' => $cart->getValues(),
-		'products' => $cart->getProducts()
+		'products' => $cart->getProducts(),
+		'error' => Cart::getMsgError()
 	]);
 });
 
@@ -86,6 +87,14 @@ $app->get('/cart/:idproduct/remove', function ($idProduct) {
 
 	$cart = Cart::getFromSession();
 	$cart->removeProduct($product, true);
+
+	header('Location: /cart');
+	exit;
+});
+
+$app->post('/cart/freight', function () {
+	$cart = Cart::getFromSession();
+	$cart->setFreight($_POST['zipcode']);
 
 	header('Location: /cart');
 	exit;
