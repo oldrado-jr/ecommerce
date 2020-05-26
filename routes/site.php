20ba092351583828affa65ba6436fdffec50763e
Oldrado Junior <oldradojunior@gmail.com>
2020-05-25 21:27:30 -0300

    Adiciona cadastro do cliente no site

--- routes/site.php
+++ routes/site.php
@@ -116,9 +116,16 @@ $app->get('/checkout', function () {
 });
 
 $app->get('/login', function () {
+	$registerValues = $_SESSION['registerValues'] ?? [
+		'name' => '',
+		'email' => '',
+		'phone' => ''
+	];
 	$page = new Page();
 	$page->setTpl('login', [
-		'error' => User::getError()
+		'error' => User::getError(),
+		'errorRegister' => User::getErrorRegister(),
+		'registerValues' => $registerValues
 	]);
 });
 
@@ -136,5 +143,47 @@ $app->post('/login', function () {
 $app->get('/logout', function () {
 	User::logout();
 	header('Location: /login');
+	exit;
+});
+
+$app->post('/register', function () {
+	$errors = [];
+
+	foreach ($_POST as $key => &$value) {
+		if ('login' != $key && 'phone' != $key && empty($value)) {
+			$errors[] = "Preencha o campo ${key}!";
+		}
+	}
+
+	if (!empty($errors) || User::checkLoginExists($_POST['email'])) {
+		$_POST = array_map('strip_tags', $_POST);
+		$_SESSION['registerValues'] = array_map('trim', $_POST);
+
+		if (!empty($errors)) {
+			User::setErrorRegister(implode('<br/>', $errors));
+		} elseif (User::checkLoginExists($_POST['email'])) {
+			User::setErrorRegister('Este endereço de e-mail já está sendo usado por outro usuário.');
+		}
+
+		header('Location: /login');
+		exit;
+	}
+
+	unset($_SESSION['registerValues']);
+
+	$user = new User();
+	$user->setData([
+		'inadmin' => false,
+		'deslogin' => $_POST['email'],
+		'desperson' => $_POST['name'],
+		'desemail' => $_POST['email'],
+		'despassword' => $_POST['password'],
+		'nrphone' => $_POST['phone']
+	]);
+	$user->save();
+
+	User::login($_POST['email'], $_POST['password']);
+
+	header('Location: /checkout');
 	exit;
 });
