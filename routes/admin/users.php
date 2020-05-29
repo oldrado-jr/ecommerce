f3dab11272cd0a52cbb06d932e16e0af37a4b0cd
Oldrado Junior <oldradojunior@gmail.com>
2020-05-29 00:19:52 -0300

    Adiciona funcionalidade de alterar senha do admin

--- routes/admin/users.php
+++ routes/admin/users.php
@@ -34,6 +34,116 @@ $app->get('/admin/logout', function () {
 	exit;
 });
 
+$app->get('/admin/users/:iduser/delete', function ($idUser) {
+	User::verifyLogin();
+
+	$user = new User();
+	$user->get($idUser);
+	$user->delete();
+
+	header('Location: /admin/users');
+	exit;
+});
+
+$app->get('/admin/users/:iduser/password', function ($idUser) {
+	User::verifyLogin();
+
+	$user = new User();
+	$user->get($idUser);
+
+	$page = new PageAdmin();
+	$page->setTpl('users-password', [
+		'msgError' => User::getError(),
+		'msgSuccess' => User::getSuccess(),
+		'user' => $user->getValues()
+	]);
+});
+
+$app->post('/admin/users/:iduser/password', function ($idUser) {
+	User::verifyLogin();
+
+	if (empty($_POST['despassword'])) {
+        User::setError('Digite a nova senha!');
+        header("Location: /admin/users/${idUser}/password");
+        exit;
+    }
+
+    if (empty($_POST['despassword-confirm'])) {
+        User::setError('Confirme a nova senha!');
+        header("Location: /admin/users/${idUser}/password");
+        exit;
+    }
+
+    if ($_POST['despassword-confirm'] !== $_POST['despassword']) {
+        User::setError('As senhas não coincidem!');
+        header("Location: /admin/users/${idUser}/password");
+        exit;
+	}
+
+	$user = new User();
+	$user->get($idUser);
+
+    if (password_verify($_POST['despassword'], $user->getDespassword())) {
+        User::setError('A sua nova senha deve ser diferente da atual!');
+        header("Location: /admin/users/${idUser}/password");
+        exit;
+    }
+
+    $user->setDespassword($_POST['despassword']);
+    $user->update();
+    User::setSuccess('Senha alterada com sucesso!');
+
+	header("Location: /admin/users/${idUser}/password");
+	exit;
+});
+
+$app->get('/admin/users/create', function () {
+	User::verifyLogin();
+	$page = new PageAdmin();
+	$page->setTpl('users-create');
+});
+
+$app->post('/admin/users/create', function () {
+	User::verifyLogin();
+
+	if (!isset($_POST['inadmin'])) {
+		$_POST['inadmin'] = 0;
+	}
+
+	$user = new User();
+	$user->setData($_POST);
+	$user->save();
+
+	header('Location: /admin/users');
+	exit;
+});
+
+$app->get('/admin/users/:iduser', function ($idUser) {
+	User::verifyLogin();
+
+	$user = new User();
+	$user->get($idUser);
+
+	$page = new PageAdmin();
+	$page->setTpl('users-update', ['user' => $user->getValues()]);
+});
+
+$app->post('/admin/users/:iduser', function ($idUser) {
+	User::verifyLogin();
+
+	if (!isset($_POST['inadmin'])) {
+		$_POST['inadmin'] = 0;
+	}
+
+	$user = new User();
+	$user->get($idUser);
+	$user->setData($_POST);
+	$user->update();
+
+	header('Location: /admin/users');
+	exit;
+});
+
 $app->get('/admin/users', function () {
 	User::verifyLogin();
 
@@ -58,62 +168,4 @@ $app->get('/admin/users', function () {
 		'search' => $search,
 		'pages' => $pages
 	]);
-});
-
-$app->get('/admin/users/create', function () {
-	User::verifyLogin();
-	$page = new PageAdmin();
-	$page->setTpl('users-create');
-});
-
-$app->get('/admin/users/:iduser/delete', function ($idUser) {
-	User::verifyLogin();
-
-	$user = new User();
-	$user->get($idUser);
-	$user->delete();
-
-	header('Location: /admin/users');
-	exit;
-});
-
-$app->get('/admin/users/:iduser', function ($idUser) {
-	User::verifyLogin();
-
-	$user = new User();
-	$user->get($idUser);
-
-	$page = new PageAdmin();
-	$page->setTpl('users-update', ['user' => $user->getValues()]);
-});
-
-$app->post('/admin/users/create', function () {
-	User::verifyLogin();
-
-	if (!isset($_POST['inadmin'])) {
-		$_POST['inadmin'] = 0;
-	}
-
-	$user = new User();
-	$user->setData($_POST);
-	$user->save();
-
-	header('Location: /admin/users');
-	exit;
-});
-
-$app->post('/admin/users/:iduser', function ($idUser) {
-	User::verifyLogin();
-
-	if (!isset($_POST['inadmin'])) {
-		$_POST['inadmin'] = 0;
-	}
-
-	$user = new User();
-	$user->get($idUser);
-	$user->setData($_POST);
-	$user->update();
-
-	header('Location: /admin/users');
-	exit;
 });
