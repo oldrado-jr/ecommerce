51735fbfcc8a5220fe33e156ff36de84a505dd27
Oldrado Junior <oldradojunior@gmail.com>
2021-02-13 04:17:49 -0300

    Refatora importação dos arquivos de rotas

--- index.php
+++ index.php
@@ -13,19 +13,9 @@ define('SITE_ROUTE', 'routes' . DIRECTORY_SEPARATOR . 'site' . DIRECTORY_SEPARAT
 define('ADMIN_ROUTE', 'routes' . DIRECTORY_SEPARATOR . 'admin' . DIRECTORY_SEPARATOR);
 
 require_once 'util' . DIRECTORY_SEPARATOR . 'functions.php';
-require_once SITE_ROUTE . 'site.php';
-require_once SITE_ROUTE . 'cart.php';
-require_once SITE_ROUTE . 'users.php';
-require_once SITE_ROUTE . 'forgot.php';
-require_once SITE_ROUTE . 'profile.php';
-require_once SITE_ROUTE . 'checkout.php';
-require_once SITE_ROUTE . 'order.php';
-require_once SITE_ROUTE . 'boleto.php';
-require_once ADMIN_ROUTE . 'users.php';
-require_once ADMIN_ROUTE . 'forgot.php';
-require_once ADMIN_ROUTE . 'categories.php';
-require_once ADMIN_ROUTE . 'categories-products.php';
-require_once ADMIN_ROUTE . 'products.php';
-require_once ADMIN_ROUTE . 'orders.php';
+
+foreach (glob('{' . SITE_ROUTE . '*.php,' . ADMIN_ROUTE . '*.php}', GLOB_BRACE) as $rota) {
+    require_once $rota;
+}
 
 $app->run();
