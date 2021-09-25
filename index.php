903de16a9f31670a63d2db14434b4e84179105f9
Oldrado Junior <oldradojunior@gmail.com>
2021-09-24 23:16:53 -0300

    Adiciona configurações de ambiente utilizando .env

--- index.php
+++ index.php
@@ -3,11 +3,13 @@ session_start();
 
 require_once 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
+use Dotenv\Dotenv;
 use Slim\Slim;
 
-$app = new Slim();
+Dotenv::createImmutable(__DIR__)->load();
 
-$app->config('debug', true);
+$app = new Slim();
+$app->config('debug', filter_var($_ENV['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN));
 
 define('SITE_ROUTE', 'routes' . DIRECTORY_SEPARATOR . 'site' . DIRECTORY_SEPARATOR);
 define('ADMIN_ROUTE', 'routes' . DIRECTORY_SEPARATOR . 'admin' . DIRECTORY_SEPARATOR);
