a32f5c2ff54f83e45f63ce417fb3ed255001458e
Oldrado Junior <oldradojunior@gmail.com>
2021-09-25 15:21:17 -0300

    Atualiza versão do Slim

--- index.php
+++ index.php
@@ -4,12 +4,15 @@ session_start();
 require_once 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 use Dotenv\Dotenv;
-use Slim\Slim;
+use Slim\App;
 
 Dotenv::createImmutable(__DIR__)->load();
 
-$app = new Slim();
-$app->config('debug', filter_var($_ENV['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN));
+$app = new App([
+	'settings' => [
+		'displayErrorDetails' => filter_var($_ENV['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN)
+	]
+]);
 
 $routesDir = 'routes' . DIRECTORY_SEPARATOR;
 $siteRoutesDir = $routesDir . 'site' . DIRECTORY_SEPARATOR;
